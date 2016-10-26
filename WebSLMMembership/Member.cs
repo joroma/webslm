369d13c00105b4a59df87d171bd8ffdfa59fb57c
JoRo Maglalang <ebild00d@gmail.com>
2016-10-26 16:35:14 +0800

    Initial commit of webslm project

---
+++ WebSLMMembership/Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSLMMembership
+{
+
+    private enum Types { regular, associate };
+
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string MemberType { get; set; }
+
+        public ICollection<MembershipFee> MemFee { get; set; }
+
+
+    }
+}
---
+++ WebSLMMembership/MembershipFee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSLMMembership
+{
+    
+
+    public class MembershipFee
+    {
+        public int MemberFeeID { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }    
+
+    }
+}
