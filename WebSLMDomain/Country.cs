34c9970a3a53c7cfa7d6e799a8bff7d4174f2b40
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-10-27 15:08:05 +0800

    Add general class to the domain

---
+++ WebSLMDomain/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSLMDomain
+{
+    class Country
+    {
+        public int CountryID { get; set; }
+        public int Description { get; set; }
+
+    }
+}
