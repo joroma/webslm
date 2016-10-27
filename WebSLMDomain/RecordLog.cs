7cd6bb194266a5affa2dd7f1605520db2fa45391
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-10-27 18:17:12 +0800

    Add RecordLog Class

---
+++ WebSLMDomain/RecordLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSLMDomain
+{
+    public class RecordLog
+    {
+        public int RecordLogID { get; set; }
+
+        public string CreationOperator { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+
+        public void AssignLog (DateTime createDate, DateTime updateDate, string Operator)
+        {
+            CreationOperator = Operator;
+            if (createDate != null)
+            {
+                CreationDate = createDate;
+            }
+            LastUpdateDate = updateDate;
+        }
+
+        
+
+    }
+}
