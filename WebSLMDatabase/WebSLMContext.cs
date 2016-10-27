7bab6e76c0577bed233b3cfea80b719800a62eff
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-10-27 18:23:16 +0800

    Add Database class library

--- WebSLMDatabase/WebSLMContext.cs
+++ WebSLMDatabase/WebSLMContext.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using WebSLMMembership;
+
 
 namespace WebSLMDatabase
 {
     class WebSLMContext : DbContext
     {
+
 
     }
 }
