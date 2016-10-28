0b7708d302a413a59391992ae1f0d81112f99421
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-10-28 16:48:35 +0800

    Add member viewmodel

---
+++ webslm/Models/ViewModels/MemberViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebSLM.Models.ViewModels
+{
+    public class MemberViewModel
+    {
+        public int MemberID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+}
---
+++ webslm/Models/ViewModels/ViewModelHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSLMMembership;
+
+namespace WebSLM.Models.ViewModels
+{
+    public static class ViewModelHelper
+    {
+        public static MemberViewModel ToViewModel (this Member member)
+        {
+            var memberViewModel = new MemberViewModel
+            {
+                FirstName = member.Firstname,
+                MemberID = member.MemberID,
+                LastName = member.Lastname
+
+            };
+            return memberViewModel;
+        }
+
+        public static Member ToDomainModel (this MemberViewModel memberViewModel)
+        {
+            var member = new Member();
+            member.Firstname = memberViewModel.FirstName;
+            member.Lastname = memberViewModel.LastName;
+            member.MemberID = memberViewModel.MemberID;
+
+
+            return member;
+
+        }
+
+    }
+}
