dd9426d44100c6568ac26c0185e9cc832ac69fe1
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-10-28 16:47:58 +0800

    Add Member controller

---
+++ WebSLM/Controllers/MemberController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSLMMembership;
+using WebSLMDatabase;
+using WebSLMDomain;
+using WebSLM.Models.ViewModels;
+
+
+
+namespace WebSLM.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly WebSLMContext db = new WebSLMContext();
+        
+        // GET: Member
+        public ActionResult Index()
+        {
+            var members = db.Members.ToList();
+            var memberViewModels = members.Select(member => member.ToViewModel()).ToList();
+            
+            return View(memberViewModels);
+        }
+    }
+}
